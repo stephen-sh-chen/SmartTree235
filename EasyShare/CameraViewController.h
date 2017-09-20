e19c783ae28a9c0541df4aa68eda30a71e2ad560
Leonard Lee <archerleonard@gmail.com>
2017-09-19 20:25:48 -0700

    add Camera View Controller in Objective C

---
+++ EasyShare/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  SmarTree_2017Summer
+//
+//  Created by Leonard Lee on 19/09/2017.
+//  Copyright © 2017 Appkoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
