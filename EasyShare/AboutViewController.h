ed8bdac06cdddc30225491c5721fea185248aa5f
Leonard Lee <archerleonard@gmail.com>
2017-09-20 11:30:58 -0700

    add About View Controller

---
+++ EasyShare/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  SmarTree_2017Summer
+//
+//  Created by Leonard Lee on 20/09/2017.
+//  Copyright © 2017 Appkoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ EasyShare/AboutViewController.m
@@ -0,0 +1,37 @@
+//
+//  AboutViewController.m
+//  SmarTree_2017Summer
+//
+//  Created by Leonard Lee on 20/09/2017.
+//  Copyright © 2017 Appkoder. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
